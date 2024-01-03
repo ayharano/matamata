f343e26b8a770ee6be66dd3350f304f067dc73b8
Alexandre Harano <email@ayharano.dev>
2024-01-03 04:30:15 -0300

    chore: test OpenAPI docs retrieval

---
+++ tests/test_app.py
@@ -0,0 +1,8 @@
+def test_api_docs_endpoint_is_accessible(client):
+    response = client.get('/docs')
+    assert response.status_code == 200
+
+
+def test_api_docs_definition_is_accessible(client):
+    response = client.get('/openapi.json')
+    assert response.status_code == 200
