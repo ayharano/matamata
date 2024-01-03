c12e4e2ea577987e592a27a43bcb5b8a48a905a3
Alexandre Harano <email@ayharano.dev>
2024-01-03 05:09:27 -0300

    chore: add database fixture as well as adjust client fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,43 @@
 import pytest
 from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
+from matamata.database import get_session
 from matamata.main import app
+from matamata.models import Base
+from matamata.settings import settings
 
 
 @pytest.fixture
-def client():
+def session():
+    engine_kwargs = {}
+    if settings.DATABASE_URL:
+        engine_args = [settings.DATABASE_URL]
+    else:
+        from sqlalchemy.pool import StaticPool
+
+        # Defaulting to in-memory SQLite
+        engine_args = ['sqlite:///:memory:']
+        engine_kwargs = {
+            'connect_args': {'check_same_thread': False},
+            'poolclass': StaticPool,
+        }
+
+    engine = create_engine(*engine_args, **engine_kwargs)
+    Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    Base.metadata.create_all(engine)
+    yield Session()
+    Base.metadata.drop_all(engine)
+
+
+@pytest.fixture
+def client(session):
+    def get_session_override():
+        return session
+
     with TestClient(app) as client:
+        app.dependency_overrides[get_session] = get_session_override
         yield client
+
+    app.dependency_overrides.clear()
