2a880b3eaf659aa834c9c613e56848cc60bc09f2
Alexandre Harano <email@ayharano.dev>
2024-01-03 05:13:04 -0300

    chore: define new TimestampedBase to track model changes

--- src/matamata/models.py
+++ src/matamata/models.py
@@ -1,5 +1,10 @@
 from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy_utils import Timestamp
 
 
 class Base(DeclarativeBase):
     pass
+
+
+class TimestampedBase(Base, Timestamp):
+    __abstract__ = True
