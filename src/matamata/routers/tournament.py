38876f8063bd4d17d079217811439f68dae9a147
Alexandre Harano <email@ayharano.dev>
2024-01-04 19:01:45 -0300

    feat: register Competitor in Tournament endpoint

--- src/matamata/routers/tournament.py
+++ src/matamata/routers/tournament.py
@@ -1,9 +1,18 @@
-from fastapi import APIRouter, Depends
+from uuid import UUID
+
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy import select
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Session
 
 from matamata.database import get_session
-from matamata.models import Tournament
-from matamata.schemas import TournamentPayloadSchema, TournamentSchema
+from matamata.models import Competitor, Tournament, TournamentCompetitor
+from matamata.schemas import (
+    TournamentCompetitorPayloadSchema,
+    TournamentCompetitorSchema,
+    TournamentPayloadSchema,
+    TournamentSchema,
+)
 
 
 router = APIRouter(prefix='/tournament', tags=['tournament'])
@@ -22,3 +31,50 @@ def create_tournament(
     session.refresh(tournament)
 
     return tournament
+
+
+@router.post('/{tournament_uuid}/competitor', response_model=TournamentCompetitorSchema, status_code=201)
+def register_competitor_in_tournament(
+    tournament_uuid: UUID,
+    competitor_payload: TournamentCompetitorPayloadSchema,
+    session: Session = Depends(get_session),
+):
+    tournament = session.scalar(
+        select(Tournament)
+        .where(Tournament.uuid == tournament_uuid)
+    )
+
+    if not tournament:
+        raise HTTPException(status_code=404, detail='Target Tournament does not exist')
+
+    if tournament.matchesCreation:
+        raise HTTPException(
+            status_code=409,
+            detail='Target Tournament has already created its matches and does not allow new Competitors registration',
+        )
+
+    competitor = session.scalar(
+        select(Competitor)
+        .where(Competitor.uuid == competitor_payload.competitor_uuid)
+    )
+
+    if not competitor:
+        raise HTTPException(status_code=404, detail='Target Competitor does not exist')
+
+    tournament_competitor = TournamentCompetitor(
+        tournament=tournament,
+        competitor=competitor,
+    )
+    session.add(tournament_competitor)
+    try:
+        session.commit()
+    except IntegrityError:
+        session.rollback()
+        raise HTTPException(
+            status_code=409,
+            detail='Target Competitor is already registered in target Tournament',
+        )
+
+    session.refresh(tournament_competitor)
+
+    return tournament_competitor
--- tests/routers/test_tournament.py
+++ tests/routers/test_tournament.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+
 BASE_URL = '/tournament'
 
 
@@ -18,4 +21,99 @@ def test_create_tournament(client):
     assert response.json() == {
         'uuid': response_uuid,
         'label': '2002 FIFA World Cup',
+    }
+
+
+REGISTER_COMPETITOR_IN_TOURNAMENT_URL_TEMPLATE = BASE_URL + '/{tournament_uuid}/competitor'
+
+
+def test_201_for_register_competitor_in_tournament(client, tournament, competitor):
+    response = client.post(
+        REGISTER_COMPETITOR_IN_TOURNAMENT_URL_TEMPLATE.format(tournament_uuid=tournament.uuid),
+        json={
+            'competitor_uuid': str(competitor.uuid),
+        },
+    )
+
+    assert response.status_code == 201
+    assert response.json() == {
+        'tournament': {
+            'uuid': str(tournament.uuid),
+            'label': tournament.label,
+        },
+        'competitor': {
+            'uuid': str(competitor.uuid),
+            'label': competitor.label,
+        },
+    }
+
+
+def test_404_for_missing_tournament_during_register_competitor_in_tournament(client, competitor):
+    response = client.post(
+        REGISTER_COMPETITOR_IN_TOURNAMENT_URL_TEMPLATE.format(
+            tournament_uuid='01234567-89ab-cdef-0123-456789abcdef',
+        ),
+        json={
+            'competitor_uuid': str(competitor.uuid),
+        },
+    )
+
+    assert response.status_code == 404
+    assert response.json() == {
+        'detail': 'Target Tournament does not exist',
+    }
+
+
+def test_409_for_started_tournament_during_register_competitor_in_tournament(client, session, tournament, competitor):
+    # Already started tournament
+    tournament.matchesCreation = datetime(year=2024, month=1, day=1)
+    tournament.numberCompetitors = 1
+    tournament.startingRound = 0
+    session.add(tournament)
+    session.commit()
+    session.refresh(tournament)
+
+    response = client.post(
+        REGISTER_COMPETITOR_IN_TOURNAMENT_URL_TEMPLATE.format(tournament_uuid=tournament.uuid),
+        json={
+            'competitor_uuid': str(competitor.uuid),
+        },
+    )
+
+    assert response.status_code == 409
+    assert response.json() == {
+        'detail': 'Target Tournament has already created its matches and does not allow new Competitors registration',
+    }
+
+
+def test_404_for_missing_competitor_during_register_competitor_in_tournament(client, tournament):
+    response = client.post(
+        REGISTER_COMPETITOR_IN_TOURNAMENT_URL_TEMPLATE.format(tournament_uuid=tournament.uuid),
+        json={
+            'competitor_uuid': '01234567-89ab-cdef-0123-456789abcdef',
+        },
+    )
+
+    assert response.status_code == 404
+    assert response.json() == {
+        'detail': 'Target Competitor does not exist',
+    }
+
+
+def test_409_for_already_registered_competitor_during_register_competitor_in_tournament(client, session, tournament, competitor):
+    tournament.competitors.append(competitor)
+    session.add(tournament)
+    session.commit()
+    session.refresh(tournament)
+
+    response = client.post(
+        REGISTER_COMPETITOR_IN_TOURNAMENT_URL_TEMPLATE.format(tournament_uuid=tournament.uuid),
+        json={
+            'competitor_uuid': str(competitor.uuid),
+        },
+    )
+
+    assert response.status_code == 409
+    assert response.json() == {
+        'detail': 'Target Competitor is already registered in target Tournament',
     }
